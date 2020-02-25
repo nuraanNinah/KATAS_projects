236f658840782cb803963ce06f6fc8c803c50930
nuraanNinah <nuraanbarnes26@gmail.com>
2020-02-25 16:48:50 +0200

    first commit

---
+++ katas.js
@@ -0,0 +1,17 @@
+function Hello() {
+    console.log("Hello Tshepo!");
+    return;
+     }
+   Hello();
+
+
+function evenOrOdd(num){
+    let remainder = num % 2;
+        if ( remainder == 0) {
+    console.log('even');
+}
+else {
+    console.log('odd')
+    }
+}
+evenOrOdd(10)
